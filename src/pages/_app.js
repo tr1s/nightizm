296f3fbf903395b2d472072e484455766b62ba4c
Tristan Lawrence <trislaw@gmail.com>
2021-12-12 21:49:35 -0500

    fix spacing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -22,19 +22,19 @@ export default function App({ Component, pageProps }) {
       <ThemeProvider theme={theme}>
         <Script id="remove-service-worker">
           {`
-          self.addEventListener('install', function(e) {
-            self.skipWaiting();
-          });
+            self.addEventListener('install', function(e) {
+              self.skipWaiting();
+            });
 
-          self.addEventListener('activate', function(e) {
-            self.registration.unregister()
-              .then(function() {
-                return self.clients.matchAll();
-              })
-              .then(function(clients) {
-                clients.forEach(client => client.navigate(client.url))
-              });
-          });
+            self.addEventListener('activate', function(e) {
+              self.registration.unregister()
+                .then(function() {
+                  return self.clients.matchAll();
+                })
+                .then(function(clients) {
+                  clients.forEach(client => client.navigate(client.url))
+                });
+            });
           `}
         </Script>
         <Component {...pageProps} />
