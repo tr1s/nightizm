7b19535552daf506f995edab3fb324d9b7192b33
Tristan Lawrence <trislaw@gmail.com>
2022-01-02 15:07:30 -0500

    fine tuned meta tags, og url router

--- src/components/helmet.js
+++ src/components/helmet.js
@@ -1,6 +1,9 @@
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 
 const Helmet = ({ title, description, image, imageDescription }) => {
+  const router = useRouter();
+  const url = router.asPath;
   const siteName = 'Nightizm';
   const twitterHandle = '@nightizm';
 
@@ -12,20 +15,21 @@ const Helmet = ({ title, description, image, imageDescription }) => {
       <meta name="description" content={description ? description : 'Seize the Night'} />
 
       {/* Open Graph */}
-      {/* <meta property="og:url" content={{`https://nightizm.com/${url}`} key="ogurl" /> */}
-      <meta
-        property="og:image"
-        content={image ? image : 'https://i.imgur.com/CMlpCzE.jpg'}
-        key="ogimage"
-      />
-      <meta property="og:image:alt" content="Sailor Moon holding a floppy disc." />
       <meta property="og:site_name" content={siteName} key="ogsitename" />
+      <meta property="og:type" content="website" key="ogtype" />
+      <meta property="og:url" content={`https://nightizm.com${url}`} key="ogurl" />
       <meta property="og:title" content={title ? title : siteName} key="ogtitle" />
       <meta
         property="og:description"
         content={description ? description : 'Seize the Night'}
         key="ogdesc"
       />
+      <meta
+        property="og:image"
+        content={image ? image : 'https://i.imgur.com/8U7TTkU.jpg'}
+        key="ogimage"
+      />
+      <meta property="og:image:alt" content="Portrait photo of Nightizm." />
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary" key="twcard" />
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,6 +10,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           rel="preload"
           href="/fonts/amarante-v8-latin-regular.woff2"
