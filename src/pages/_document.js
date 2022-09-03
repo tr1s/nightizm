5431d2d2bd49fe34bd30c2bb972ed74a579439e8
Tristan Lawrence <trislaw@gmail.com>
2022-09-03 01:22:47 -0400

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,4 +33,59 @@ export default class MyDocument extends Document {
       sheet.seal();
     }
   }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+          <link
+            rel="preload"
+            href="/fonts/amarante-v8-latin-regular.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/pt-serif-v12-latin-regular.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/pt-serif-v12-latin-700.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/pt-serif-v12-latin-italic.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/pt-serif-v12-latin-700italic.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/MagdaCleanMono-Regular.woff"
+            as="font"
+            type="font/woff"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
