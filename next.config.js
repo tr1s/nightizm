9cadd0f4df508408ce2c5da3d4e64d4374e1c084
Tristan Lawrence <trislaw@gmail.com>
2022-09-03 00:43:42 -0400

    ignore all eslint

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ module.exports = {
   compiler: {
     styledComponents: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     urlImports: ['https://cdn.skypack.dev'],
   },
--- src/components/helmet.js
+++ src/components/helmet.js
@@ -1,5 +1,3 @@
-/* eslint-disable react/no-unknown-property */
-
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 
