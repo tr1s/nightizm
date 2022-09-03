ed57c6928014e867bc10ee728864f04602b26a73
Tristan Lawrence <trislaw@gmail.com>
2022-09-03 11:08:19 -0400

    upgrade to react 18

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,4 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-  DocumentInitialProps,
-} from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
